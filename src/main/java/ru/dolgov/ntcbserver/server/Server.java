961b10feb0864f6cbae7c74cef9141229a12abbd
dolgowmm <dolgowmm@gmail.com>
2017-11-09 23:28:42 +0700

    work with Server class

--- src/main/java/ru/dolgov/ntcbserver/server/Server.java
+++ src/main/java/ru/dolgov/ntcbserver/server/Server.java
@@ -1,5 +1,34 @@
 package ru.dolgov.ntcbserver.server;
 
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
 public class Server {
-    
+
+    private int port;
+
+    public Server(int port) {
+        this.port = port;
+    }
+
+    public void run() throws Exception {
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try{
+            ServerBootstrap server = new ServerBootstrap();
+            server.group(bossGroup, workerGroup).channel(NioServerSocketChannel.class)
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        public void initChannel(SocketChannel channel) throws Exception {
+                            channel.pipeline().addLast();
+                        }
+                    });
+        } finally {
+
+        }
+    }
 }
