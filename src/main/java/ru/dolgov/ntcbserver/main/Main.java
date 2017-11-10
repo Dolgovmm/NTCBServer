9c9ee28bf9f904f3ce4a79f9fac4af62c434de72
dolgowmm <dolgowmm@gmail.com>
2017-11-10 14:26:06 +0700

    test start server

--- src/main/java/ru/dolgov/ntcbserver/main/Main.java
+++ src/main/java/ru/dolgov/ntcbserver/main/Main.java
@@ -1,7 +1,13 @@
 package ru.dolgov.ntcbserver.main;
 
+import ru.dolgov.ntcbserver.server.Server;
+
 public class Main {
     public static void main(String[] args) {
-
+        try {
+            new Server(9000).run();
+        }catch (Exception ex) {
+            ex.printStackTrace();
+        }
     }
 }
--- src/main/java/ru/dolgov/ntcbserver/server/Server.java
+++ src/main/java/ru/dolgov/ntcbserver/server/Server.java
@@ -1,15 +1,17 @@
 package ru.dolgov.ntcbserver.server;
 
 import io.netty.bootstrap.ServerBootstrap;
-import io.netty.channel.ChannelInitializer;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelOption;
 import io.netty.channel.EventLoopGroup;
-import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 
 public class Server {
 
     private int port;
+    private ChannelFuture channelFuture;
 
     public Server(int port) {
         this.port = port;
@@ -21,14 +23,15 @@ public class Server {
         try{
             ServerBootstrap server = new ServerBootstrap();
             server.group(bossGroup, workerGroup).channel(NioServerSocketChannel.class)
-                    .childHandler(new ChannelInitializer<SocketChannel>() {
-                        @Override
-                        public void initChannel(SocketChannel channel) throws Exception {
-                            channel.pipeline().addLast();
-                        }
-                    });
-        } finally {
+                    .childHandler(new SocketChannelInitializer())
+                    .option(ChannelOption.SO_BACKLOG, 128)
+                    .childOption(ChannelOption.SO_KEEPALIVE, true);
+            channelFuture = server.bind().sync();
 
+            channelFuture.channel().closeFuture().sync();
+        } finally {
+            workerGroup.shutdown();
+            bossGroup.shutdown();
         }
     }
 }
